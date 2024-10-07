077d0d458d7532f8070e477d1067054ef10a2fd1
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-10-07 18:04:26 +0200

    ✨dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
-    <>
+    <main className="">
       <Navbar />
       <Outlet />
-    </>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,9 @@ export default {
   theme: {
     extend: {},
   },
+  darkMode: ["class", '[data-theme="dark"]'],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
   plugins: [daisyui],
 };
