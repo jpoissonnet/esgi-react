007e09324020136fb3cab600ed1df38de46a5023
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2025-02-02 13:54:45 +0100

    allow preview cleverdomain i hope

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: "app-9cb8c7f8-92a1-44d0-911e-159498a660bf.cleverapps.io",
     port: 8080,
+    allowedHosts: [".cleverapps.io"],
   },
 });
